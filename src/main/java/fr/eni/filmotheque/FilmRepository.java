5e8dfec4a4ad228c95fb358eb299076f132214f7
Alx <91890825+AlexBrlt@users.noreply.github.com>
2022-10-03 15:29:19 +0200

    gradle bis

--- src/main/java/fr/eni/filmotheque/FilmRepository.java
+++ src/main/java/fr/eni/filmotheque/FilmRepository.java
@@ -1,6 +1,8 @@
 package fr.eni.filmotheque;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import fr.eni.filmotheque.bo.Film;
 
 public interface FilmRepository extends JpaRepository<Film, Integer>{
 
